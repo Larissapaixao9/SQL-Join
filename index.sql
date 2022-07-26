fa8c9cdee6bd3c0177f69d90f1650f0f04c5bc65
Larissa Paixao <laryssapaixao9@gmail.com>
2022-07-26 19:19:37 -0300

    Finaliza segunda questão

--- index.sql
+++ index.sql
@@ -1,4 +1,11 @@
+--Questao 1:
 SELECT users.name, cities.name as city FROM users JOIN cities ON users."cityId"=cities.id WHERE cities.name='Rio de Janeiro';
+
+--Questão 2:
+SELECT testimonials.id, testimonials.message, users_one.name as writer, users_two.name as recipient FROM testimonials JOIN users AS users_one ON testimonials."writerId"=users_one.id JOIN users AS users_two on testimonials."recipientId"=users_two.id;
+
+--Questao 3:
+
 
 
 
